907d03ed3aafa3a1220db51a2c75c0f5b900f478
Shad <shad@chaz.pro>
2025-02-06 19:50:53 +0100

    feat: added test render login page

--- ea-platform/ea-front/__tests__/root.test.tsx
+++ ea-platform/ea-front/__tests__/root.test.tsx
@@ -1,9 +1,14 @@
-import '@testing-library/jest-dom'
-import { render, screen } from '@testing-library/react'
-import Page from '../app/page'
- 
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import RootPage from '../app/page';
+import LoginPage from '../app/login/page';
+
 describe('Page', () => {
   it('renders the root page', () => {
-    render(<Page />)
-  })
+    render(<RootPage />)
+  });
+
+  it('renders the login page', () => {
+    render(<LoginPage />)
+  });
 })
