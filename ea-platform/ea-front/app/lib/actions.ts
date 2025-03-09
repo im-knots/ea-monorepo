738e1c1132856e11cb8594e35ae10aef28d835cd
Shad <shad@chaz.pro>
2025-03-09 23:38:27 +0100

    :construction: moved lib/ dir inside app/

---
+++ ea-platform/ea-front/app/lib/actions.ts
@@ -0,0 +1,56 @@
+'use server';
+
+import bcrypt from 'bcryptjs';
+import { z } from 'zod';
+import { prisma } from "@/app/lib/prisma";
+import { Login, Register } from './auth';
+import { redirect } from 'next/navigation';
+
+const LoginForm = z.object({
+  email: z.string().email(),
+  password: z.string(),
+});
+
+export async function register(prevState: string | undefined, formData: FormData) {
+  const validatedForm = LoginForm.safeParse({
+    email: formData.get('email'),
+    password: formData.get('password'),
+  });
+  if (!validatedForm.success) {
+    return "Invalid form data";
+  }
+  try {
+    await Register({
+      email: validatedForm.data.email,
+      password: validatedForm.data.password
+    });
+    await Login({
+      email: validatedForm.data.email,
+      password: validatedForm.data.password
+    })
+    redirect('/dashboard');
+  } catch (error) {
+     console.error('Registration error:', error);
+     return "Server error";
+  }
+}
+
+export async function login(prevState: string | undefined,formData: FormData) {
+  const validatedForm = LoginForm.safeParse({
+    email: formData.get('email'),
+    password: formData.get('password'),
+  });
+  if (!validatedForm.success) {
+    return "Invalid form data";
+  }
+  try {
+    Login({
+      email: validatedForm.data.email,
+      password: validatedForm.data.password
+    });
+    redirect('/dashboard');
+  } catch (error) {
+     console.error('Login error:', error);
+     return "Server error";
+  }
+}
---
+++ ea-platform/ea-front/app/lib/auth.ts
@@ -0,0 +1,57 @@
+import bcrypt from "bcryptjs";
+import { prisma } from "./prisma";
+import jwt from "jsonwebtoken";
+import { cookies } from "next/headers";
+
+export async function Register(user: { email: string, password: string }) {
+  const { email, password } = user;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  return await prisma.user.create({
+    data: {
+      email,
+      password: hashedPassword,
+    },
+  });
+}
+
+export async function Login(user: { email: string, password: string }) {
+  const cookieStore = await cookies();
+
+  const { email, password } = user;
+  
+  if (!process.env.JWT_SECRET) {
+    throw new Error("JWT_SECRET not set");
+  }
+  
+  const userRecord = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (!userRecord) {
+    throw new Error("User not found");
+  }
+
+  const passwordMatch = await bcrypt.compare(password, userRecord.password);
+
+  if (!passwordMatch) {
+    throw new Error("Invalid password");
+  }
+ 
+  const token = jwt.sign({
+      userId: userRecord.id,
+      email,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: '6h'
+    }
+  );
+  cookieStore.set('token', token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'strict',
+  })
+  return token;
+}
