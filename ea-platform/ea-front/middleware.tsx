dce37102b0feab19cf0a5e1a5b0333d70dcf8659
Shad <shad@chaz.pro>
2025-03-09 23:39:41 +0100

    :sparkles: Added /register page with JWT based authent

--- ea-platform/ea-front/middleware.tsx
+++ ea-platform/ea-front/middleware.tsx
@@ -1,54 +1,34 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { jwtVerify } from 'jose';
 
-const JWT_SECRET = process.env.JWT_SECRET || 'supersecretkey';
-const JWT_SECRET_KEY = new TextEncoder().encode(JWT_SECRET); // Convert to Uint8Array
-
 export async function middleware(req: NextRequest) {
+  if (!process.env.JWT_SECRET) {
+    throw new Error('JWT_SECRET not set');
+  }
   const token = req.cookies.get('token')?.value;
+  const JWT_SECRET_KEY = new TextEncoder().encode(process.env.JWT_SECRET);
 
-  console.log('Middleware running...');
-  console.log('Token:', token);
-  console.log('Path:', req.nextUrl.pathname);
-
-  const isAuthRoute = req.nextUrl.pathname === '/login';
-
-  // Manually define a list of protected routes
-  const protectedRoutes = [
-    '/dashboard',
-    '/agent-builder',
-    '/agent-manager',
-    '/node-builder',
-    '/datasets',
-  ];
-
-  const isProtectedRoute = protectedRoutes.some((route) =>
-    req.nextUrl.pathname.startsWith(route)
-  );
+  const unprotectedRoutes = ['/login', '/register'];
+  const isProtectedRoute = !unprotectedRoutes.includes(req.nextUrl.pathname);
 
   try {
+    if (!token && isProtectedRoute) {
+      console.log('No valid token. Redirecting to /login...');
+      return NextResponse.redirect(new URL('/login', req.url));
+    }
     if (token) {
-      const { payload } = await jwtVerify(token, JWT_SECRET_KEY);
-
-      console.log('JWT Verified:', payload);
-
-      // If logged-in user tries to access /login, redirect them to dashboard
-      if (isAuthRoute) {
-        console.log('User already logged in. Redirecting to /dashboard...');
-        return NextResponse.redirect(new URL('/dashboard', req.url));
-      }
-
+      jwtVerify(token, JWT_SECRET_KEY);
       return NextResponse.next();
     }
   } catch (error) {
     console.log('Invalid or expired token:', error);
-  }
-
-  // If no valid token and trying to access a protected route, redirect to login
-  if (!token && isProtectedRoute) {
-    console.log('No valid token. Redirecting to /login...');
     return NextResponse.redirect(new URL('/login', req.url));
   }
-
   return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+     '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ]
 }
